725bfbbd9e86d3a5b54874e374a3f442974ab543
Macaronj <74501326+macaronj@users.noreply.github.com>
2025-11-25 16:04:25 +0100

    JSON Encoding

--- cmd/api/movies.go
+++ cmd/api/movies.go
@@ -15,6 +15,6 @@ func (app *application) showMovieHandler(w http.ResponseWriter, r *http.Request)
 		http.NotFound(w, r)
 		return
 	}
-
+	
 	fmt.Fprintf(w, "show the details of movie %d\n", id)
 }
--- go.mod
+++ go.mod
@@ -2,4 +2,4 @@ module greenlight.macaronj
 
 go 1.25.4
 
-require github.com/julienschmidt/httprouter v1.3.0 // indirect
+require github.com/julienschmidt/httprouter v1.3.0
