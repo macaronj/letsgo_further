223ead8f0ed1a85898c2ebac6a28cdac84165943
Macaronj <74501326+macaronj@users.noreply.github.com>
2025-11-26 16:32:10 +0100

    4-2: Managing Bad Requests

--- cmd/api/helpers.go
+++ cmd/api/helpers.go
@@ -3,6 +3,8 @@ package main
 import (
 	"encoding/json"
 	"errors"
+	"fmt"
+	"io"
 	"net/http"
 	"strconv"
 
@@ -41,5 +43,44 @@ func (app *application) writeJSON(w http.ResponseWriter, status int, data any, h
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(status)
 	w.Write(js)
+	return nil
+}
+
+func (app *application) readJSON(w http.ResponseWriter, r *http.Request, dst any) error {
+	err := json.NewDecoder(r.Body).Decode(dst)
+	if err != nil {
+		// Starting triage
+		var syntaxError *json.SyntaxError
+		var unmarshalTypeError *json.UnmarshalTypeError
+		var invalidUnmarshallError *json.InvalidUnmarshalError
+
+		switch {
+		// *syntaxError
+		case errors.As(err, &syntaxError):
+			return fmt.Errorf("body contains badly-formatted JSON (at %d)", syntaxError.Offset)
+
+			// In some circumstances Decode() may also return an io.ErrUnexpectedEOF error
+			// for syntax errors in the JSON. So we check for this using errors.Is() and
+			// return a generic error message. There is an open issue regarding this at
+			// https://github.com/golang/go/issues/25956
+		case errors.Is(err, io.ErrUnexpectedEOF):
+			return errors.New("body contains badly-formed JSON")
+
+			// Wrong JSON value type
+		case errors.As(err, &unmarshalTypeError):
+			if unmarshalTypeError.Field != "" {
+				return fmt.Errorf("body contains incorret JSON type for field %q", unmarshalTypeError.Field)
+			}
+			return fmt.Errorf("body contains incorrect JSON type (at character %d)", unmarshalTypeError.Offset)
+			// Request body is empty
+		case errors.Is(err, io.EOF):
+			return errors.New("body must not be empty")
+			// Non-nil pointer => unexpected error => panic
+		case errors.As(err, &invalidUnmarshallError):
+			panic(err)
+		default:
+			return err
+		}
+	}
 	return nil
 }
