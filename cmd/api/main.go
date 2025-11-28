27bacc89c886cec6d647e09827faf6185da423b5
Macaronj <74501326+macaronj@users.noreply.github.com>
2025-11-28 16:51:59 +0100

    7.1: Setting up the Movie Model

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -9,10 +9,12 @@ import (
 	"net/http"
 	"os"
 	"time"
+
 	// Import the pq driver so that it can register itself with the database/sql
 	// package. Note that we alias this import to the blank identifier, to stop the Go
 	// compiler complaining that the package isn't being used.
 	_ "github.com/lib/pq"
+	"greenlight.macaronj/internal/data"
 )
 
 // INFO: Version number, will be generated at build time later on
@@ -35,6 +37,7 @@ type config struct {
 type application struct {
 	config config
 	logger *slog.Logger
+	models data.Models
 }
 
 func main() {
@@ -43,34 +46,29 @@ func main() {
 	// Reading flags
 	flag.IntVar(&cfg.port, "port", 4000, "API Server port")
 	flag.StringVar(&cfg.env, "env", "development", "Enviroment (developemt|staging|production)")
-
-	// Read the DSN value from the db-dsn command-line flag into the config struct. We
-	// default to using our development DSN if no flag is provided.
 	flag.StringVar(&cfg.db.dsn, "db-dsn", os.Getenv("GREENLIGHT_DB_DSN"), "PostgreSQL DSN")
-
-	// Read the connection pool settings from command-line flags into the config struct.
-	// Notice that the default values we're using are the ones we discussed above?
 	flag.IntVar(&cfg.db.maxOpenConns, "db-max-open-conns", 25, "PostgreSQL max open connections")
 	flag.IntVar(&cfg.db.maxIdleConns, "db-max-idle-conns", 25, "PostgreSQL max idle connections")
 	flag.DurationVar(&cfg.db.maxIdleTime, "db-max-idle-time", 15*time.Minute, "PostgreSQL max connection idle time")
-
 	// Parsing flags
 	flag.Parse()
 
-	// Initialite a new structured logger
 	logger := slog.New(slog.NewTextHandler(os.Stdout, nil))
 
-	// Declare instance of application struct
+	db, err := openDB(cfg)
+	if err != nil {
+		logger.Error(err.Error())
+		os.Exit(1)
+	}
+	defer db.Close()
+	logger.Info("database connection pool established")
+
 	app := &application{
 		config: cfg,
 		logger: logger,
+		models: data.NewModels(db),
 	}
 
-	// Declare a new servemux and a /v1/healthcheck route
-	mux := http.NewServeMux()
-	mux.HandleFunc("/v1/healthcheck", app.healthcheckHandler)
-
-	// Declare http server
 	srv := &http.Server{
 		Addr:         fmt.Sprintf(":%d", cfg.port),
 		Handler:      app.routes(),
@@ -80,26 +78,6 @@ func main() {
 		ErrorLog:     slog.NewLogLogger(logger.Handler(), slog.LevelError),
 	}
 
-	// Start http server
-	logger.Info("starting server", "addr", srv.Addr, "env", cfg.env)
-
-	// Call the openDB() helper function to create the connection pool,
-	// passing in the config struct. If this returns an error, we log it and exit the
-	// application immediately.
-	db, err := openDB(cfg)
-	if err != nil {
-		logger.Error(err.Error())
-		os.Exit(1)
-	}
-
-	// Defer a call to db.Close() so that the connection pool is closed before the
-	// main() function exits.
-	defer db.Close()
-
-	// Also log a message to say that the connection pool has been successfully
-	// established.
-	logger.Info("database connection pool established")
-
 	err = srv.ListenAndServe()
 	logger.Error(err.Error())
 	os.Exit(1)
@@ -107,39 +85,23 @@ func main() {
 
 // The openDB() function returns a sql.DB connection pool.
 func openDB(cfg config) (*sql.DB, error) {
-	// Use sql.Open() to create an empty connection pool, using the DSN from the config
-	// struct.
 	db, err := sql.Open("postgres", cfg.db.dsn)
 	if err != nil {
 		return nil, err
 	}
 
-	// Set the maximum number of open (in-use + idle) connections in the pool. Note that
-	// passing a value less than or equal to 0 will mean there is no limit.
 	db.SetMaxOpenConns(cfg.db.maxOpenConns)
-
-	// Set the maximum number of idle connections in the pool. Again, passing a value
-	// less than or equal to 0 will mean there is no limit.
 	db.SetMaxIdleConns(cfg.db.maxIdleConns)
-
-	// Set the maximum idle timeout for connections in the pool. Passing a duration less
-	// than or equal to 0 will mean that connections are not closed due to their idle time.
 	db.SetConnMaxIdleTime(cfg.db.maxIdleTime)
 
-	// Create a context with a 5-second timeout deadline.
 	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
 	defer cancel()
 
-	// Use PingContext() to establish a new connection to the database, passing in the
-	// context we created above as a parameter. If the connection couldn't be
-	// established successfully within the 5 second deadline, then this will return an
-	// error. If we get this error, or any other, we close the connection pool and
-	// return the error.
 	err = db.PingContext(ctx)
 	if err != nil {
 		db.Close()
 		return nil, err
 	}
-	// Return the sql.DB connection pool.
+
 	return db, nil
 }
