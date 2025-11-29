6eb393ca461c9b25a85737cc9f12c28b69535a35
Macaronj <74501326+macaronj@users.noreply.github.com>
2025-11-29 19:58:12 +0100

    8-2: Optimistic Concurrency Control

--- internal/data/movies.go
+++ internal/data/movies.go
@@ -83,7 +83,7 @@ func (m MovieModel) Get(id int64) (*Movie, error) {
 
 // Update changes a specific record in the movies table.
 func (m MovieModel) Update(movie *Movie) error {
-	query := "UPDATE movies SET title = $1, year = $2, runtime = $3, genres = $4, version = version + 1 WHERE id = $5 RETURNING version "
+	query := "UPDATE movies SET title = $1, year = $2, runtime = $3, genres = $4, version = version + 1 WHERE id = $5 AND version = $6 RETURNING version "
 
 	args := []any{
 		movie.Title,
@@ -91,9 +91,19 @@ func (m MovieModel) Update(movie *Movie) error {
 		movie.Runtime,
 		pq.Array(movie.Genres),
 		movie.ID,
+		movie.Version,
 	}
 
-	return m.DB.QueryRow(query, args...).Scan(&movie.Version)
+	err := m.DB.QueryRow(query, args...).Scan(&movie.Version)
+	if err != nil {
+		switch {
+		case errors.Is(err, sql.ErrNoRows):
+			return ErrEditConflict
+		default:
+			return err
+		}
+	}
+	return nil
 }
 
 // Delete removes a specific record from the movies table.
