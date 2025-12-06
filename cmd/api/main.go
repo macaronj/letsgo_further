d63ad24531e9b06d70c57324c0b4b18f269edddb
Macaronj <74501326+macaronj@users.noreply.github.com>
2025-12-06 18:55:09 +0100

    18.2: Creating Custom Metrics

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -3,9 +3,11 @@ package main
 import (
 	"context"
 	"database/sql"
+	"expvar"
 	"flag"
 	"log/slog"
 	"os"
+	"runtime"
 	"strings"
 	"sync"
 	"time"
@@ -107,6 +109,21 @@ func main() {
 		logger.Error(err.Error())
 		os.Exit(1)
 	}
+
+	// Expvar - Piblic variables
+	expvar.NewString("version").Set(version)
+
+	expvar.Publish("goroutines", expvar.Func(func() any {
+		return runtime.NumGoroutine()
+	}))
+
+	expvar.Publish("database", expvar.Func(func() any {
+		return db.Stats()
+	}))
+
+	expvar.Publish("timestamp", expvar.Func(func() any {
+		return time.Now().Unix()
+	}))
 
 	app := &application{
 		config: cfg,
